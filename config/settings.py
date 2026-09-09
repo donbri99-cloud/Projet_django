084ab23f04c83477d59ace9d16dda2c10f8df87a
Rielo77 <donbri99@example.com>
2026-09-09 10:00:39 +0300

    appli

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
+    'products',
+    'posts',
+    'articles',
 ]
 
 MIDDLEWARE = [
