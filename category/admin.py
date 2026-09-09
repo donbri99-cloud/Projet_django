b9fb42707a7ce91b501800574565e5deee0c905f
Arsene-Faly <arsenefaly1@gmail.com>
2026-09-09 10:30:22 +0300

    Categories

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "is_active", "created_at")
+    search_fields = ("name",)
+    list_filter = ("is_active",)
+    readonly_fields = ("slug", "created_at", "updated_at")
